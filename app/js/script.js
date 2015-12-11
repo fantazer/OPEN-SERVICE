56cf44dbbebe2645bef52413339a0f76538cd380
fantazer <fantazer@text.ru>
2015-12-11 14:09:00 +0300

    add small grid bootstrap, imports stylus, js-helpers

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,65 @@
+/* ###### init skrol to point  ######*/
+/* ###### bower i page-scroll-to-id  ######*/
+/*(function($){
+    $(window).load(function(){
+        $("a[rel='m_PageScroll2id']").mPageScroll2id({
+				    offset:200
+				});
+    });
+ })(jQuery);*/ 
+
 $(document).ready(function(){
-	$("#owl-example").owlCarousel({
-	 	items : 1,
-	 	autoHeight : true,
-	 	pagination : false,
-	 	autoPlay : true,
-	 	singleItem:true
-	 	}
-	 ); 	
+
+	/* ###### init stickUp  ######*/
+	/* ###### bower i sticky  ######*/
+	/*$("#sticker").sticky({topSpacing:0});*/
+
+
+	/* ###### init owl-carousel  ######*/
+	/* ###### bower i owlcar ######*/
+	// $("#owl-example").owlCarousel({
+	//  	items : 1,
+	//  	autoHeight : true,
+	//  	pagination : false,
+	//  	autoPlay : true,
+	//  	singleItem:true
+	//  	}
+	//  ); 	
+
+	/* ###### init validate form  ######*/
+	/* ###### bower i jquery-validation ######*/
+	/*$('#myform').validate({
+			rules:{ //правила для полей 
+				name:{
+					required:true,
+					minlength:5 //минимальное значение поля
+				},
+				phone:{
+					required:true,
+					number:true
+				}
+			},
+			messages:{
+				name:{
+					required: 'Это поле обязатлеьно для заполнения', //какое сообщение будет выводиться
+					minlength:'Имя должно быть не меньше 5 символов'
+				},
+				phone:{
+					required: 'Это поле обязатлеьно для заполнения',
+					number:'Введите правильный телефон'
+				},
+				submitHandler:function(){ //выполнять если все валидно
+					alert('Форма заполнена правильно');
+				}
+			}
+	})*/
+
+	/* ###### init animatedModal  ######*/
+	/* ###### bower i animatedModa  ######*/
+	// $(".play").animatedModal({
+	//  	 animatedIn:'lightSpeedIn',
+	//     animatedOut:'bounceOutDown',
+	//     color:'#0394c7'
+	//  	});
+	
 })
