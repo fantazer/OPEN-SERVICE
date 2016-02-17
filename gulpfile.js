56e3cc38215accd0e6a5aec2a575b7d4b9e0fdd7
Your Name <you@example.com>
2016-02-17 16:38:44 +0300

    add cache + fast build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var ftpConf = {
-  "user":"u6929655",
-  "pass":"YDW2wJI4",
+  "user":"",
+  "pass":"",
   "name":"catalog"
 }
 
@@ -27,31 +27,11 @@ var notify = require('gulp-notify');
 var pngquant = require('imagemin-pngquant');
 var imageminMozjpeg = require('imagemin-mozjpeg');
 var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cached');
+var remember = require('gulp-remember');
 
 
-//Prefix my css
-gulp.task('prefix', function () {
-    return gulp.src('app/css/style.css')
-        .pipe(autoprefixer({
-            browsers: ['last 15 versions']
-        }))
-        .pipe(gulp.dest('app/css/'));
-});
-
-//Show error
-function errorhandler(a) {
-    var args = Array.prototype.slice.call(arguments);
-    // Send error to notification center with gulp-notify
-    notify.onError({
-        title: 'Compile Error',
-        message: a,
-        // sound: true можно даже со звуком!
-    }).apply(this, args);
-
-    // Keep gulp from hanging on this task
-    this.emit('end');
-};
-
+// ########## make img ###############
 gulp.task('imagePng',function(){
  return gulp.src('app/img/*.png')
      .pipe(imagemin({
@@ -72,12 +52,108 @@ gulp.task('imageJpg',function(){
   .pipe(gulp.dest('dist/img/'));
 });
 
+// ########## make css ###############
+
+//Prefix my css
+gulp.task('prefix', function () {
+    return gulp.src('app/css/style.css')
+        .pipe(autoprefixer({
+            browsers: ['last 15 versions']
+        }))
+        .pipe(gulp.dest('app/css/'));
+});
+
+//Stylus
+gulp.task('stylus', function () {
+  return gulp.src('./app/css/*.styl',{since:gulp.lastRun('stylus')})
+    .pipe(cache('stylus'))
+    .pipe(stylus({
+        use:[rupture(),axis(),jeet()]
+        })).on('error', errorhandler)
+    .pipe(gulp.dest('./app/css/'))
+  
+});
+
+//Source map
+gulp.task('sourcemaps', function () {
+  return gulp.src('./app/css/style.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus({
+        use:[rupture(),axis(),jeet()]
+        }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./app/css/'))
+});
+
+// ########## make css end ###############
+
+// ########## make html ###############
+
+//Include html
+gulp.task('fileinclude', function() {
+  gulp.src('./app/html/*.html')
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: './app/html/block_html/'
+    }))
+    .pipe(gulp.dest('./app/'));
+});
+
+//Jade
+gulp.task('jade', function() {
+  return gulp.src('./app/html/**/**.jade')
+    .pipe(cache('jade'))
+    .pipe(data( function(file) {
+            return require('./app/html/data.json');
+    } ))
+    .pipe(jade({
+      pretty: true
+    }).on('error', errorhandler))
+    .pipe(gulp.dest('./app/'))
+});
+
+gulp.task('include',function(){
+        gulp.watch('app/html/**/*.html',gulp.series('fileinclude'))
+})
+// ########## make html end###############
+
+
+// ########## make service###############
+//copy file
+gulp.task('copy:font',function(){
+  return gulp.src('./app/fonts/')
+         .pipe(gulp.dest('./dist/fonts/'))
+  })
+
+gulp.task('copy:js',function(){
+  return gulp.src('./app/js/script.js')
+         .pipe(gulp.dest('./dist/js/'))
+  })
+
+//Show error
+function errorhandler(a) {
+    var args = Array.prototype.slice.call(arguments);
+    // Send error to notification center with gulp-notify
+    notify.onError({
+        title: 'Compile Error',
+        message: a,
+        // sound: true можно даже со звуком!
+    }).apply(this, args);
+
+    // Keep gulp from hanging on this task
+    this.emit('end');
+};
 
 //useref
 gulp.task('make', function () {
   var assets = useref.assets();
    gulp.src('app/js/*.js')
+  .pipe(uglify())
   .pipe(gulp.dest('dist/js/'));
+
+   gulp.src('app/css/style.css')
+  .pipe(minifyCss())
+  .pipe(gulp.dest('dist/css/'));
   return gulp.src('app/*.html')
       .pipe(assets)
       .pipe(gulpif('*.js', uglify()))
@@ -106,60 +182,7 @@ gulp.task( 'ftp', function() {
 
 } );
 
-
-//Stylus
-gulp.task('stylus', function () {
-  gulp.src('./app/css/*.styl')
-    .pipe(stylus({
-        use:[rupture(),axis(),jeet()]
-        })).on('error', errorhandler)
-    .pipe(gulp.dest('./app/css/'))
-});
-
-//Source map
-gulp.task('sourcemaps', function () {
-  return gulp.src('./app/css/style.styl')
-    .pipe(sourcemaps.init())
-    .pipe(stylus({
-        use:[rupture(),axis(),jeet()]
-        }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./app/css/'))
-});
-
-//Include html
-gulp.task('fileinclude', function() {
-  gulp.src('./app/html/*.html')
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: './app/html/block_html/'
-    }))
-    .pipe(gulp.dest('./app/'));
-});
-
-//Jade
-gulp.task('jade', function() {
-  gulp.src('./app/html/**/**.jade')
-    .pipe(data( function(file) {
-            return require('./app/html/data.json');
-    } ))
-    .pipe(jade({
-      pretty: true
-    }).on('error', errorhandler))
-    .pipe(gulp.dest('./app/'))
-});
-
-//Watcher
-gulp.task('see',function(){
-        gulp.watch('app/html/**/*.jade',gulp.series('jade'))
-        gulp.watch('app/css/*.styl',gulp.series('stylus'))
-})
-
-gulp.task('include',function(){
-        gulp.watch('app/html/**/*.html',gulp.series('fileinclude'))
-})
-
-
+//LiveReload
 gulp.task('serve', function () {
     browserSync.init({
         notify: false,
@@ -170,11 +193,26 @@ gulp.task('serve', function () {
     browserSync.watch(["./app/css/**/*.css","./app/*.html","./app/js/**.*"]).on("change", browserSync.reload);
 });
 
+// ########## make service end ###############
+
+//Watcher
+gulp.task('see',function(){
+        gulp.watch('app/html/**/*.jade',gulp.series('jade'))
+        gulp.watch('app/css/*.styl',gulp.series('stylus'))
+})
 
 //default
 gulp.task('img',gulp.series( 'imagePng' , 'imageJpg'));
 gulp.task('default', gulp.parallel('serve','see'));
 gulp.task('build',gulp.series('sourcemaps','copy:font','prefix','img','make'));
+gulp.task('fast-build',gulp.series('stylus','prefix','jade','copy:js','ftp'));
+
+
+
+gulp.task('fast-see',function(){
+        gulp.watch(["./app/css/**/**.styl","./app/html/**.jade","./app/js/**.*"] ,gulp.series('fast-build'))
+})
+
 
 //gulp.task('default', function() {
 //  runSequence('see');
